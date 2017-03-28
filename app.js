1183279490061ef9c877f4d99e5d735e1b00ae84
cnsheafe <cnsheafe@gmail.com>
2017-03-28 14:13:04 -0500

    replaced function pointers in report object with calculated values. Using sets for unique-word count

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
+/*jshint esversion: 6*/
 function HandleSubmit() {
   $('form').on('submit', function(ev) {
     ev.preventDefault();
     $('dl').addClass('hidden');
-    var textInput = $(this).serializeArray()[0].value;
-    var report = makeReport(textInput);
-    var children = $('.hidden').find('dd');
+    const textInput = $(this).serializeArray()[0].value;
+    const report = makeReport(textInput);
+    const children = $('.hidden').find('dd');
     children.eq(0).text(report.wordCt);
-    children.eq(1).text(report.unqCt());
-    children.eq(2).text(report.wordAvg());
+    children.eq(1).text(report.unqCt);
+    children.eq(2).text(report.wordAvg);
     $('.hidden').removeClass('hidden');
   });
 }
-/*Example of closure:
-tkText can still be accessed even after makeReport is finished*/
+
 function makeReport(text) {
 
-  tkText = text.split(/\W+/);
+  tkText = text.toLowerCase().split(/\W+/);
+  let wordCt = tkText.length;
   if (tkText[tkText.length-1] === ''){
     tkText.pop();
+    wordCt--;
   }
 
-  var report = {
-    wordCt: tkText.length,
-
-    unqCt: function() {
-      var unqCt = 0;
-      for (var i=0; i < this.wordCt; i++) {
-        if(tkText.lastIndexOf(tkText[i]) === tkText.indexOf(tkText[i])) {
-          unqCt++;
-        }
-      }
-      return unqCt;
-    },
-
-    wordAvg: function() {
-      var sum = 0;
-      for (var  i = 0; i < this.wordCt; i++) {
-        sum += tkText[i].length;
-      }
-      return sum/this.wordCt;
+  let unqWords = new Set();
+  for (let i = 0; i < wordCt; i++) {
+      unqWords.add(tkText[i]);
     }
+
+  let sum = 0;
+  for (let  i = 0; i < wordCt; i++) {
+    sum += tkText[i].length;
+  }
+
+  const report = {
+    wordCt: wordCt,
+    unqCt: unqWords.size,
+    wordAvg: sum/wordCt
   };
 
   return report;
